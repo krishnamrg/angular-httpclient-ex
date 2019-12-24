01a3ee67c3d0740751fe906ffdeb619af7bd652b
Krishnam G <krishnam.goud@ask.com>
2019-12-24 12:22:41 +0530

    added service and typed response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule, MatProgressSpinnerModule } 
+from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule, 
+    MatButtonModule, 
+    MatProgressSpinnerModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpResponse } from '@angular/common/http'
+import { DataService } from '../service/data.service';
+import { Product } from '../product';
+
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  products = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.sendRequest().pipe(takeUntil(this.destroy$)).subscribe(
+      (res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
+  }
+
+  public firstPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.first)
+      .pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+  }
+  public previousPage() {
+    if (this.dataService.prev !== undefined && this.dataService.prev !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.prev)
+        .pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+          console.log(res);
+          this.products = res.body;
+        })
+    }
+  }
+  public nextPage() {
+    if (this.dataService.next !== undefined && this.dataService.next !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.next)
+        .pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+          console.log(res);
+          this.products = res.body;
+        })
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.last)
+      .pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+  }
+
+}
